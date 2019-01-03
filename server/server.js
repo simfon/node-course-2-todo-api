61b036fba5b106e93f6b9220d1afdcd6934b6bbb
Simone Fontana <sim.fontana@gmail.com>
2019-01-03 09:35:53 +0100

    Test POST route

---
+++ server/server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+   var todo = new Todo(
+       {
+           text: req.body.text
+       }
+   ).save().then((doc) => {
+        res.send(doc);
+        console.log('Saved');
+   }).catch((err) => {
+       res.status(400).send(err);
+   })
+
+});
+
+app.listen(3000, () => {
+    console.log('Starting on port 3000');
+})
+
+module.exports = {app};
