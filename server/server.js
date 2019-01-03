22ac707a10be05db5ff2c0116ff5edd750849328
Simone Fontana <sim.fontana@gmail.com>
2019-01-03 15:45:59 +0100

    ops

--- server/server.js
+++ server/server.js
@@ -118,21 +118,6 @@ app.post('/users', (req, res) => {
     });
 });
 
-var authenticate = (req, res, next) => {
-    var token = req.header('x-auth');
-    
-    User.findByToken(token).then((user) => {
-        if(!user){
-            return Promise.reject();
-        }
-        req.user = user;
-        req.token = token;
-        next();
-    }).catch((err) => {
-        res.status(401).send();
-    });
-}
-
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
