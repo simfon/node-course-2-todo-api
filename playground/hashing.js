2821220c83d337e9381faef6acdeb764d9da9b6f
Simone Fontana <sim.fontana@gmail.com>
2019-01-03 16:09:23 +0100

    hash pass

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
- var data = {
+var password = '123abca';
+
+/* bcrypt.genSalt(15, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    })
+}) */
+
+bcrypt.genSalt(15).then((salt) => {
+    bcrypt.hash(password, salt).then((hash) => {
+        console.log(hash);
+        bcrypt.compare(password, hash, (err, res) => {
+            console.log(res);
+        });
+    });
+}).catch((err) => {
+    console.log(err);
+});
+
+
+
+/*  var data = {
      id: '5c2df620f63a8914ec37d034'
  };
 
@@ -10,7 +32,7 @@ console.log(token);
 
 var decoded = jwt.verify(token + 1, '123abc');
 console.log('decoded', decoded);
-
+ */
 // var message = 'I am user number 3';
 
 // var hash = SHA256(message).toString();
