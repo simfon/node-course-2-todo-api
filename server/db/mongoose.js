ac915d0d4d59d5e3e6a86498a1241b546dd908ba
Simone Fontana <sim.fontana@gmail.com>
2019-01-03 10:49:22 +0100

    Setup for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,12 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }).catch((err) => {
+var local = 'mongodb://localhost:27017/TodoApp';
+var heroku = 'mongodb://heroku_csxrfxrp:ehc2s9nr8kruqquavm6eeb8ce8@ds247944.mlab.com:47944/heroku_csxrfxrp';
+
+var db = process.env.MONGODB_URI || local;
+
+mongoose.connect(db , { useNewUrlParser: true }).catch((err) => {
     console.log(err);
 });
 
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
+
 
 app.use(bodyParser.json());
 
@@ -54,8 +56,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Starting on port 3000');
+app.listen(port, () => {
+    console.log(`Starting on port ${port}`);
 })
 
 module.exports = {app};
