5e87e71daa73573bb4dc0fa785c2edcf639cc5a8
Simone Fontana <sim.fontana@gmail.com>
2019-01-04 09:19:35 +0100

    login tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -253,4 +253,57 @@ describe('POST /user', (done) => {
             .expect(400)
             .end(done);
     });
-})
+});
+
+describe('POST /users/login', () => {
+
+    it('should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: 'second@user.com',
+                password: 'userTwoPass'
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+            })
+            .end((err, res) => {
+                if (err){
+                    return done(err);
+                }
+
+                User.findById(users[1]).then((user => {
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                })).catch((e) => done(e));
+            })
+    });
+ 
+    it('should reject invalid login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: 'second@user.com',
+                password: 'asdfa'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+                if (err){
+                    return done(err);
+                }
+
+                User.findById(users[1]).then((user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                })).catch((e) => done(e));
+            })
+    }); 
+
+});
